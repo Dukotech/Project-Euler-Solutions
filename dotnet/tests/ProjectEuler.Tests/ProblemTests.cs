995322e2710508556d487b6b9c900ea27bc09d0c
oguncukur <cukurogun@gmail.com>
2018-10-30 16:03:04 +0300

    Problem13 solved

--- dotnet/tests/ProjectEuler.Tests/ProblemTests.cs
+++ dotnet/tests/ProjectEuler.Tests/ProblemTests.cs
@@ -9,20 +9,19 @@ namespace ProjectEuler.Tests
         [Fact]
         public void Promlem4_ShouldReturn_906609()
         {
-<<<<<<< HEAD
             Assert.Equal(Problem4.LargestThreeDigitPalindrome(), 906609);
         }
 
         [Fact]
         public void Problem8_ShouldReturn_23514624000()
-=======
-            Assert.Equal(Problem4.LargestThreeDigitPalindrome(),906609);            
+        {
+            Assert.Equal(Problem4.LargestThreeDigitPalindrome(), 906609);
         }
-        
+
         [Fact]
         public void Problem2_ReturnResult_4613732()
         {
-            Assert.Equal(Problem2.FibonacciEvenNumbersSum(0,1,0),4613732);
+            Assert.Equal(Problem2.FibonacciEvenNumbersSum(0, 1, 0), 4613732);
         }
 
         //[Fact] 
@@ -33,7 +32,6 @@ namespace ProjectEuler.Tests
 
         [Fact]
         public void Promlem7_ShouldReturn_104743()
->>>>>>> upstream/master
         {
             Assert.Equal(Problem7.Problem7Answer(), 104743);
         }
@@ -43,27 +41,24 @@ namespace ProjectEuler.Tests
         {
             Assert.Equal(Problem16.Problem16Answer(), 1366);
         }
-      
+
         [Fact]
         public void Problem20_ShouldReturn_648()
         {
             Assert.Equal(Problem20.FactorialDigitSum(), 648);
         }
-<<<<<<< HEAD
 
         [Fact]
         public void Problem13_ShouldReturn_5537376230()
         {
             Assert.Equal(Problem13.FirstTenDigit(), "5537376230");
         }
-=======
-      
+
         [Fact]
         public void Promlem9_ShouldReturn_31875000()
         {
-            Assert.Equal(Problem9.Pythagorean(),31875000);
+            Assert.Equal(Problem9.Pythagorean(), 31875000);
         }
 
->>>>>>> upstream/master
     }
 }
