8de5ce3943605c69519ca17bc5a5f457b9f78a49
Emrullah AĞOLDAY <emrullahagolday@hotmail.com>
2018-10-31 10:39:03 +0300

    problem 14 tquestion text updated

--- dotnet/src/Problem14.cs
+++ dotnet/src/Problem14.cs
@@ -4,18 +4,21 @@ using System.Text;
 
 namespace ProjectEuler
 {
-    //The following iterative sequence is defined for the set of positive integers:
-    //n → n/2 (n is even)
-    //n → 3n + 1 (n is odd)
-
-    //Using the rule above and starting with 13, we generate the following sequence:
-
-    //13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-    //It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
-    //Which starting number, under one million, produces the longest chain?
-
-    //NOTE: Once the chain starts the terms are allowed to go above one million.
+    /*
+     * Question: The following iterative sequence is defined for the set of positive integers:
+     * 
+     * n → n/2 (n is even)
+     * n → 3n + 1 (n is odd)
+     * 
+     * Using the rule above and starting with 13, we generate the following sequence:
+     * 
+     * 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+     * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+     * 
+     * Which starting number, under one million, produces the longest chain?
+     * 
+     * NOTE: Once the chain starts the terms are allowed to go above one million.
+     */
 
     /// <summary>
     /// 1000000 başlayıp bu sayı içerisinde tek ise (n → 3n + 1)  çift ise (n → n/2) bu formül uygulanacak ve sayı 1 de bitmeli. bu sayılardan en uzun halka hangi sayıda olacak hadi bulalım :D 
