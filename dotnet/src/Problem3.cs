bf6c91ddf7241f3696d591209832d2fa8b3f1420
MeJuniorW <murithiwagura@gmail.com>
2021-04-02 18:09:23 +0300

    Update Problem3.cs

--- dotnet/src/Problem3.cs
+++ dotnet/src/Problem3.cs
@@ -11,7 +11,52 @@ namespace ProjectEuler
     {
         public static int GetResult()
         {
-            throw new NotImplementedException();
+            long numberTest = 600851475143;
+            List<int> factors = GetFactors(numberTest);
+            List<int> primeFactors = new List<int>();
+            foreach (var num in factors)
+            {
+                if (IsPrime(num))
+                {
+                    primeFactors.Add(num);
+                }
+            }
+            int largestNumber = 0;
+            foreach (var number in primeFactors)
+            {
+                int temp = largestNumber;
+                largestNumber = number;
+                if (temp > largestNumber)
+                {
+                    largestNumber = temp;
+                }
+            }
+            return largestNumber;
+        }
+        static List<int> GetFactors(long number)
+        {
+            List<int> factors = new List<int>();
+            List<int> primeFactors = new List<int>();
+            for (var i = 1; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+                    factors.Add(i);
+                }
+            }
+            return factors;
+        }
+
+        static bool IsPrime(int num)
+        {
+            if((GetFactors(num).Count == 1) && (num!=1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
